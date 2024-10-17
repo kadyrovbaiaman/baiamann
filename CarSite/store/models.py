3174ead68b3b712bfeace6f219ba01d0e926fefb
rahmanovdev <jumadilrahmonov@gmail.com>
2024-10-17 16:07:30 +0600

    fnj

--- CarSite/store/models.py
+++ CarSite/store/models.py
@@ -34,7 +34,7 @@ class Car(models.Model):
     drive = models.CharField(default='задный', max_length=16, choices=CHOICES_DRIVE)
 
     CHOICES_ENGINE = (
-        ('бензин', 'бензин'),
+        ('бензин', 'бенн'),
         ('газ', 'газ'),
         ('дизель', 'дизель'),
         ('электрический', 'электрический')
